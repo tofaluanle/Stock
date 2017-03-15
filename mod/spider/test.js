0f687e4f07933e872bc9180c4218b2efc3df58cc
songjj <songjj@ennew.cn>
2017-03-15 16:20:23 +0800

    完成了CodeInfo表的插入和更新功能

--- mod/spider/test.js
+++ mod/spider/test.js
@@ -2,11 +2,30 @@
  * Created by songjj on 2017/3/7.
  */
 
-var MetaBean = require('../../lib/database/MetaBean.js');
-var meta = new MetaBean('page', 1);
+var CodeInfoBean = require('../../lib/database/CodeInfoBean.js');
+var codeInfo = new CodeInfoBean('symbol', 'test3');
+codeInfo.trade = 1.2;
+codeInfo.pricechange = 1.3;
+codeInfo.changepercent = 1.4;
+codeInfo.buy = 1.5;
+codeInfo.sell = 1.6;
+codeInfo.settlement = 1.7;
+codeInfo.open = 1.8;
+codeInfo.high = -0.1;
+codeInfo.low = -0.2;
+codeInfo.volume = 3;
+codeInfo.amount = 4;
+codeInfo.code = '123456';
+codeInfo.ticktime = '15:33:111';
 // console.log(meta);
-var metaDao = require('../../lib/database/MetaDao.js');
-metaDao.save(meta);
+var codeInfoDao = require('../../lib/database/CodeInfoDao.js');
+codeInfoDao.save(codeInfo);
+
+// var MetaBean = require('../../lib/database/MetaBean.js');
+// var meta = new MetaBean('page2', 3);
+// // console.log(meta);
+// var metaDao = require('../../lib/database/MetaDao.js');
+// metaDao.save(meta);
 
 
 var str = "{\"error_code\":0,\"reason\":\"SUCCESSED!\",    \"result\":{\"totalCount\":\"1220\",\"page\":\"1\",\"num\":\"20\",\"data\":[{\"symbol\":\"sh600000\",\"name\":\"浦发银行\",\"trade\":\"16.430\",\"pricechange\":\"0.020\",\"changepercent\":\"0.122\",\"buy\":\"16.430\",\"sell\":\"16.440\",\"settlement\":\"16.410\",\"open\":\"16.380\",\"high\":\"16.450\",\"low\":\"16.370\",\"volume\":93198,\"amount\":152969168,\"code\":\"600000\",\"ticktime\":\"15:00:00\"}]}}";
